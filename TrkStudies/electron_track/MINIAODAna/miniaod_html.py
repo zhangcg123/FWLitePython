b73b22b9b1c8f73a7db53ca2ec6bcb637704b0e6
zhangcg123 <zhangchenguang18@gmail.com>
2022-07-17 16:12:50 +0200

    update

--- TrkStudies/electron_track/MINIAODAna/miniaod_html.py
+++ TrkStudies/electron_track/MINIAODAna/miniaod_html.py
@@ -11,10 +11,23 @@ dirs = DirTree()
 dirs.mkrootdir('test_'+os.getcwd().split('/')[-1])
 path = dirs.root
 
+# load json to pandas dataframe
+df = pd.read_json('miniaod_Zee.json',orient='index')
 
-df = pd.read_json('aod_Zee.json',orient='index')
+# custom columns order
+colstmp = []
+with open('columns.txt','r') as colsf:
+	for l in colsf:
+		colstmp.append(l.strip())
+df = df[colstmp]
 
+# custom index order
+# custom by index. sort_index returns the index order as 0,1,10,100,1000.... do not konw why. if you want 1,2,3..., as following two lines.
+#indextmp = [i for i in range(0,len(df.index))]
+#df.index = indextmp
 
+# custom index by columns values, by lumiblock and eventNum here.
+df.sort_values(by=['LumiBlock','eventNum'],inplace=True)
 
 myhtml = HTMLClass('Z to ee info for electron track study')
 myhtml.section('')
